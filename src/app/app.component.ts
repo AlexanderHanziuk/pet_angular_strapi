0420864d0e79faab252640723b650eb4d6e89d0a
AlexanderHanziuk <aleksandrtiskov488@gmail.com>
2024-09-23 15:52:06 +0300

    create about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/components/header/header.component';
 import { HomeComponent } from './pages/components/home/home.component';
+import { AboutComponent } from './pages/components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/components/about/about.component.ts
+++ src/app/pages/components/about/about.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {
-
+  public description: string =
+    "In this section I'll tell you a little about myself. I was born and live in Minsk, Belarus. I'm 25 years old and I'm full of strength and energy to make this world a better place. I started working on development a little over a year ago and now I'm 100% sure that this is my calling. If you're interested in learning more about me, click on the button below.";
 }
