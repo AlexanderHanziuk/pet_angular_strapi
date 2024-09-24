00672298b1f25261ca90f6c275afd9a44887a526
AlexanderHanziuk <aleksandrtiskov488@gmail.com>
2024-09-24 14:48:41 +0300

    add burger menu switching functionality

--- src/app/pages/components/header/header.component.ts
+++ src/app/pages/components/header/header.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isBurgerOpen: boolean = false;
+
+  public toggleBurgerMenu(): void {
+    this.isBurgerOpen = !this.isBurgerOpen;
+  }
+}
